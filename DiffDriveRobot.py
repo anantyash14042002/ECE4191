34c95bc7b2f7f474570f53d05bfd950f099d51b3
Kah Kai <kahkaikhoo@gmail.com>
2024-08-16 12:35:36 +1000

    Added controller and drive signals

--- DiffDriveRobot.py
+++ DiffDriveRobot.py
@@ -31,17 +31,34 @@ class Controller:
     
     ## Play around with gain values
     def __init__(self, Kp = 0.1, Ki = 0.01, wheel_radius = 0.05, wheel_sep = 0.15):
+
+        # Gains
         self.Kp = Kp
         self.Ki = Ki
 
-
+        # Wheel radius and separation
         self.r = wheel_radius
         self.l = wheel_sep
 
         # Initialise error term for integral control
-        self.e_l = 0.0      # Left wheel
-        self.e_r = 0.0      # Right wheel
+        self.e_left = 0.0      # Left wheel
+        self.e_right = 0.0      # Right wheel
 
 
-    def generate_control(self):
-        pass
+    def get_control(self, w_desired, w_measured, error):
+        duty_cycle = np.clip(self.kp*(w_desired-w_measured) + self.Ki*error, -1.0, 1.0) # Clip value to between -1 and 1
+
+        error_new = error + (w_desired-w_measured) # Update error term
+
+        return duty_cycle, error_new
+    
+    # Generate drive signals for left and right wheel
+    def drive(self, v_desired, w_desired, w_left, w_right):
+
+        wl_desired = v_desired/self.r + self.l*w_desired/2 
+        wr_desired = v_desired/self.r - self.l*w_desired/2
+        
+        duty_cycle_l, self.e_left = self.get_control(wl_desired,w_left, self.e_left)
+        duty_cycle_r, self.e_right = self.get_control(wr_desired,w_right, self.e_right)
+
+        return duty_cycle_l, duty_cycle_r

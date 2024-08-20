470d7ad8d7c775e4cafd66e19320b7e275cffb1f
Kah Kai <kahkaikhoo@gmail.com>
2024-08-20 13:24:25 +1000

    Added functions for scale and baseline estimation

--- callibration/wheel_callibration.py
+++ callibration/wheel_callibration.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 import sys
 sys.path.insert(0, "../util")
-from pibot import PenguinPi
+# Import robot
 
 
 
@@ -15,41 +15,37 @@ def calibrateWheelRadius():
     ##########################################
     # Feel free to change the range / step
     ##########################################
-    wheel_velocities_range = range(20, 80, 15)
-    delta_times = []
+    wheel_velocities_range = range(20, 80, 15) # Ticks/s
+    time = 5 #Fixed drive time in seconds
+    scale =0
 
-    for wheel_vel in wheel_velocities_range:
-        print("Driving at {} ticks/s.".format(wheel_vel))
-        # Repeat the test until the correct time is found.
+    for vel in wheel_velocities_range:
+        print("Driving at {} ticks/s".format(vel))
+
+        # Drive the robot at the given speed for the given time
+        # TODO: Insert command for robot to drive at vel for time 
+
         while True:
-            delta_time = input("Input the time to drive in seconds: ")
-            try:
-                delta_time = float(delta_time)
-            except ValueError:
-                print("Time must be a number.")
-                continue
+            dist = input("Enter the distance travelled in meters: ")
 
-            # Drive the robot at the given speed for the given time
-            ppi.set_velocity([1, 0], tick=wheel_vel, time=delta_time)
+            # Check that input is a number
+            try: dist = float(dist)
+            except ValueError: print("Distance must be a number")
 
-            uInput = input("Did the robot travel 1m?[y/N]")
+            
+            uInput = input(f"Confirm robot travelled {dist}m? [y/N]")
             if uInput == 'y':
-                delta_times.append(delta_time)
-                print("Recording that the robot drove 1m in {:.2f} seconds at wheel speed {}.\n".format(delta_time,
-                                                                                                        wheel_vel))
+                scale += dist/(vel*time)
+                print(f"Recording that robot drove {dist}m in {time} seconds at wheel speed {vel}")
                 break
-
-    # Once finished driving, compute the scale parameter by averaging
-    num = len(wheel_velocities_range)
-    scale = 0
-    for delta_time, wheel_vel in zip(delta_times, wheel_velocities_range):
-        pass # TODO: replace with your code to compute the scale parameter using wheel_vel and delta_time
-        
-        scale += wheel_vel*delta_time
-    scale =1/ (scale/num)
-    print("The scale parameter is estimated as {:.6f} m/ticks.".format(scale))
-
+    
+    scale = scale / len(wheel_velocities_range) # Average the measurements
+    print(f"The scale is estimated to be {scale:.6f} m/tick")
     return scale
+
+
+
+
 
 
 def calibrateBaseline(scale):
@@ -62,59 +58,52 @@ def calibrateBaseline(scale):
     # Feel free to change the range / step
     ##########################################
     wheel_velocities_range = range(30, 60, 10)
-    delta_times = []
-
-    for wheel_vel in wheel_velocities_range:
-        print("Driving at {} ticks/s.".format(wheel_vel))
-        # Repeat the test until the correct time is found.
-        while True:
-            delta_time = input("Input the time to drive in seconds: ")
-            try:
-                delta_time = float(delta_time)
-            except ValueError:
-                print("Time must be a number.")
-                continue
-
-            # Spin the robot at the given speed for the given time
-            ppi.set_velocity([0, 1], tick=20,turning_tick=wheel_vel, time = delta_time)
-
-            uInput = input("Did the robot spin 360deg?[y/N]")
-            if uInput == 'y':
-                delta_times.append(delta_time)
-                print("Recording that the robot spun 360deg in {:.2f} seconds at wheel speed {}.\n".format(delta_time,
-                                                                                                           wheel_vel))
-                break
-
-    # Once finished driving, compute the basline parameter by averaging
-    num = len(wheel_velocities_range)
+    time = 5 # Fxied time in seconds
     baseline = 0
-    for delta_time, wheel_vel in zip(delta_times, wheel_velocities_range):
-        pass # TODO: replace with your code to compute the baseline parameter using scale, wheel_vel, and delta_time
-        baseline += (scale  *  wheel_vel * delta_time)/np.pi 
-    baseline = baseline / num
-    print("The baseline parameter is estimated as {:.6f} m.".format(baseline))
 
+    for vel in wheel_velocities_range:
+        print("Driving at {} ticks/s.".format(vel))
+
+        # Rotate the robot at the given speed for the given time
+        # TODO: Insert command for robot to rotate at vel for time 
+
+        while True:
+            theta = input("Input the angle in degrees: ")
+            try: theta = float(theta)
+            except ValueError:
+                print("Angle must be a number")
+
+            uInput = input(f"Confirm robot turned {theta}degrees? [y/N]")
+            if uInput == 'y':
+                baseline += (scale * vel * time * theta)/(360*np.pi) # TODO: Check this formula
+                print(f"Recording that robot turned {theta}degrees in {time} seconds at wheel speed {vel}")
+                break
+    
+    bseline = baseline / len(wheel_velocities_range) # Take average
+    print(f"The baseline is estimated to be {baseline:.6f}m")
     return baseline
+            
+           
 
+            
 
 if __name__ == "__main__":
-    import argparse
+    # import argparse
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--ip", metavar='', type=str, default='localhost')
-    parser.add_argument("--port", metavar='', type=int, default=40000)
-    args, _ = parser.parse_known_args()
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("--ip", metavar='', type=str, default='localhost')
+    # parser.add_argument("--port", metavar='', type=int, default=40000)
+    # args, _ = parser.parse_known_args()
 
-    ppi = PenguinPi(args.ip,args.port)
+    # ppi = PenguinPi(args.ip,args.port)
 
     # calibrate pibot scale and baseline
     dataDir = "{}/param/".format(os.getcwd())
 
-    print('Calibrating PiBot scale...\n')
+    print('Calibrating scale...\n')
     scale = calibrateWheelRadius()
     fileNameS = "{}scale.txt".format(dataDir)
     np.savetxt(fileNameS, np.array([scale]), delimiter=',')
-    # scale = 0.002620373403
 
     print('Calibrating PiBot baseline...\n')
     baseline = calibrateBaseline(scale)
